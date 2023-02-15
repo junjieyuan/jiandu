4734ee42fae6c35bf578e09d01931de81a5b7ae3
原俊杰 <yuan@junjie.pro>
2023-02-16 01:12:54 +0800

    创建仓库。

---
+++ src/main/kotlin/pro/junjie/jiandu/JianduApplication.kt
@@ -0,0 +1,11 @@
+package pro.junjie.jiandu
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JianduApplication
+
+fun main(args: Array<String>) {
+	runApplication<JianduApplication>(*args)
+}
---
+++ src/test/kotlin/pro/junjie/jiandu/JianduApplicationTests.kt
@@ -0,0 +1,13 @@
+package pro.junjie.jiandu
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JianduApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
